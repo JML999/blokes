15401346d35ecf60a2720e234efed7061053f5c0
Justin Lee <Jlee3424@gmail.com>
2024-05-28 17:01:20 -0700

    Metadata Update

--- src/frontend/components/Home.js
+++ src/frontend/components/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { ethers } from 'ethers';
 import './Home.css';
-import pinkBackground from './Renders/Purple_Background.png';
+import pinkBackground from './Renders/tip2.png';
 
 const Home = ({ web3Handler, account, disconnectHandler, provider, blokes }) => {
   const [mintStatus, setMintStatus] = useState('');
